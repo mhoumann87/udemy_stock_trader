2a014344a5c2a3a5f75107c935854dac97cb6d9d
Michael Houmann <michael@michael-h.dk>
2018-11-21 14:11:29 +0100

    First commit, initialization of project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container">
+  <h1>The Stock Trader</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
